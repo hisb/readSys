e64748e4a60b2c1fb06813a9a019608e3983f2c3
yalingc <yalingc@oupeng.com>
2017-03-09 10:30:05 +0800

    缓存

--- public/javascripts/input.js
+++ public/javascripts/input.js
@@ -1,8 +1,8 @@
 $(function() {
-	$('input.input__field').on('focus', function() {
+	$('.input__field').on('focus', function() {
 		$(this).parent('span.input').addClass('input--filled');
 	});
-	$('input.input__field').on('blur', function() {
+	$('.input__field').on('blur', function() {
 		if(! $(this).val().trim()) {
 			$(this).parent('span.input').removeClass('input--filled');
 		}
